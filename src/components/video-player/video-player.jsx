7badc36b75a18633c0e66b0f72f3e1b84bc98cdb
krxxl <krxxl@mail.ru>
2020-10-13 16:56:29 +0400

    q

--- src/components/video-player/video-player.jsx
+++ src/components/video-player/video-player.jsx
@@ -4,37 +4,44 @@ export default class VideoPlayer extends PureComponent {
   constructor(props) {
     super(props);
 
+    this.videoref = React.createRef();
+
     this.state = {
       isLoading: true,
-      isPlaying: props.isPlaying,
+      isPlaying: this.props.isPlaying,
     };
   }
 
   componentDidMount() {
-    const {src} = this.props;
+    const video = this.videoref.current;
+    video.src = this.props.video;
+    video.muted = false;
+  }
 
-    this._audio = new Video(src);
+  componentDidUpdate() {
+    const video = this.videoref.current;
+    if (this.state.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  }
 
-    this._audio.oncanplaythrough = () => this.setState({
-      isLoading: false,
-    });
-
-    this._audio.onplay = () => {
-      this.setState({
-        isPlaying: true,
-      });
-    };
-
-    this._audio.onpause = () => this.setState({
-      isPlaying: false,
-    });
+  componentWillUnmount() {
+    const video = this.videoref.current;
+    video.oncanplaythrough = null;
+    video.onplay = null;
+    video.onpause = null;
+    video.src = null;
+    video = null;
   }
 
   render() {
-    var {video, preview} = this.props
+    var {preview} = this.props;
     return (
       <video
-          src={video}
+          ref={this.videoref}
+
           className="player__video"
           poster={preview}
         ></video>
